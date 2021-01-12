fd87ff38e5a332ac40deb8991a81e2b7031108bd
hajara bibi <40460495+Hajaraabibi@users.noreply.github.com>
2021-01-12 13:01:22 +0000

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,21 @@
+myName = input("what is your name? ")
+number = (1,100)
+print(myName + ", think of your favourite number between 1 and 100")
+favouritenum = int(input("type it in to hear a joke: "))
+
+if favouritenum <=20:
+    print("Q: Why did the computer squeak? A: Because someone stepped on it's mouse!")
+elif favouritenum <=40:
+    print("Q: What did the spider do on the computer? A: Made a website!")
+elif favouritenum <=60:
+    print("Q: Why was the computer cold? A: It left it's Windows open!")
+elif favouritenum <=80:
+    print("Q: What do you get when you cross a computer and a life guard? A: A screensaver!")
+elif favouritenum <=100:
+    print("Q: What did the computer do at lunchtime? A: Had a byte!")
+else:
+    print ("try again")
+
+
+
+
